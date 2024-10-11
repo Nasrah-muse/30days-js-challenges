091712edd985636df51a46c1b92312e1a03c7732
Nasrah-muse <nazronafta143@gmail.com>
2024-10-11 19:58:10 +0300

    addded day-19 js- challenges

---
+++ Day-19/main.js
@@ -0,0 +1,9 @@
+//   Checking if an array is a subst of another array
+
+function isArraySubset(array1, array2){
+    return array2.every(Element => array1.includes(Element));
+}
+
+console.log(isArraySubset([1,2,3,4,5], [2,3,5]));
+console.log(isArraySubset([100,200,300], [101,22,4]));
+console.log(isArraySubset([34,56,78], [56,78,34]));
